5561f48539e371374183179eecebdc2efb12c08c
Rat <33757638+staf-dierickx@users.noreply.github.com>
2021-12-03 13:52:53 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install ffplay espeak -y
+
+
+lspci
+
+#intresting links
+#https://askubuntu.com/questions/432740/how-to-send-receive-files-via-bluetooth-using-commands-terminal
+#https://stackoverflow.com/questions/41006556/accept-bluetooth-pairing-request-automatically-without-gui
